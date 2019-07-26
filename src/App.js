bd4f8fec77c88dedf223d3fa2fd63dc8d2206b9b
Dan Cross <dan.cross@intouchnetworks.com>
2019-07-26 09:41:24 +0100

    added banner ad

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import useDfpSlot from './components/dfp'
 import './App.css';
+import DynamicAd from './components/dynamicAd';
 
-function App() {
+const App = () => {
+  useDfpSlot({
+    path: '/21737763597/adunit-1',
+    size: [320, 100],
+    id: 'div-gpt-ad-1563785376756-0',
+  });
+
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
-    </div>
-  );
-}
+    <Fragment>
+
+      <div
+        id="div-gpt-ad-1563785376756-0"
+        style={{ width: '320px', height: '100px' }}
+      />
+
+      <button onClick={() => setToggle(!toggle)} style={{ height: '60px', width: '120px' }} >Click Me</button>
+
+      {toggle ? (<DynamicAd />) : null}
+
+    </Fragment>
+  )
+};
 
 export default App;
---
+++ src/components/dynamicAd.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const DynamicAd = () => {
+    return (
+        <Fragment>
+            <h1>TEST AD WILL GO HERE</h1>
+        </Fragment>
+    );
+}
+
+export default DynamicAd;
